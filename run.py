7e51d83f5cb46d93abc032c23b7fe4ec8c46c26c
canvas.chenc <canvas.chenc@canvaschencdeMacBook-Pro.local>
2025-11-16 20:56:30 +0800

    init

--- run.py
+++ run.py
@@ -24,4 +24,9 @@ if __name__ == "__main__":
         print("✅ 摄像头状态监听器已在后台启动")
     # --- 后台线程启动状态监听器 ---
 
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(
+        debug=True, 
+        host='0.0.0.0', 
+        port=8080,
+        ssl_context=('cert.pem', 'key.pem')
+    )
